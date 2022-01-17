fac112a97f7229086676656d3d1df23f75fc6a41
Coyaks Perez <49325924+Coyaks@users.noreply.github.com>
2022-01-17 01:39:09 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,29 +1,19 @@
-<?php 
-
-const DB_HOST="localhost";
-const DB_USER="root";
-const DB_PASSWORD="";
-const DB_NAME="db_condominio";
-//conexión básica
-try {
-    $conexion=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    $conexion->set_charset("utf8");
-    //echo "Conexion exitosa";
-} catch (Exception $ex) {
-    echo "Error al conectar: ".$ex->getMessage();
-}
-
-class Conexion{	  
-    public static function Conectar() {      
-        $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
-        try{
-            $conexion2 = new PDO("mysql:host=".DB_HOST."; dbname=".DB_NAME, DB_USER, DB_PASSWORD, $opciones);		
-            return $conexion2;
-            //echo '<script>alert("Conexion exitosa")</script>';
-        }catch (Exception $e){
-            die("El error de Conexión es: ". $e->getMessage());
+<?php
+    class Conexion{
+        public function conectar(){
+            $DB_HOST='localhost';
+            $DB_USER='root';
+            $DB_PASSWORD='';
+            $DB_NAME='prueba_usuario';
+            
+            $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');	
+            try{
+                //crear una instancia de PDO
+                $conexion= new PDO("mysql:host=".$DB_HOST."; dbname=".$DB_NAME, $DB_USER, $DB_PASSWORD, $opciones);		
+                return $conexion;
+            }catch(Exception $e){
+                die('Error de conexion: '.$e->getMessage());
+            }
         }
     }
-}
-
 ?>
